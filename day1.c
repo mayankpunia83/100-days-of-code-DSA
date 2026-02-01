ed9e2673f9fcc8c925a2482d47d1c55b343b11bc
Mayank <mayankpunia83@gmail.com>
2026-02-01 21:28:25 +0530

    day1.c

--- day1.c
+++ day1.c
@@ -1,3 +1,4 @@
+//Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
 #include <stdio.h>
 
 int main()
@@ -30,3 +31,4 @@ int main()
 
     return 0;
 }
+
